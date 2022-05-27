36cdbf53c3e5da4f8056c5f89057419027420b5a
dheeraj <80874729+dheerajhemachandran@users.noreply.github.com>
2022-05-27 19:40:41 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,40 @@
+import pytesseract 
+import cv2  
+import imutils
+import numpy as np
+
+path=r'/home/dheeraj/projects/react/anpr/test1.jpg'
+img = cv2.imread(path)
+cv2.imshow("image",img)
+cv2.waitKey(0)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
+edged = cv2.Canny(bfilter, 30, 200)
+
+keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(keypoints)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+location = None
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 10, True)
+    
+    if len(approx) == 4:
+        location = approx
+        break
+mask = np.zeros(gray.shape, np.uint8)
+new_image = cv2.drawContours(mask, [location], 0,255, -1)
+new_image = cv2.bitwise_and(img, img, mask=mask)
+
+cv2.imshow("result",cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
+cv2.waitKey(0)
+
+# (x,y) = np.where(mask==255)
+# (x1, y1) = (np.min(x), np.min(y))
+# (x2, y2) = (np.max(x), np.max(y))
+# cropped_image = gray[x1:x2+1, y1:y2+1]
+
+
+
+# predicted_res = pytesseract.image_to_string(cropped_image) 
+# print(predicted_res)
